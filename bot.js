2521778b543b1851534c9660483eedae1f3b87d3
Oskicz2005 <44738487+Oskicz2005@users.noreply.github.com>
2018-11-04 04:16:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+    static void UpdatePresence()
+    {
+        DiscordRichPresence discordPresence;
+        memset(&discordPresence, 0, sizeof(discordPresence));
+        discordPresence.state = "Playing Solo";
+        discordPresence.details = "Competitive";
+        discordPresence.startTimestamp = 1507665886;
+        discordPresence.endTimestamp = 1507665886;
+        discordPresence.largeImageText = "Numbani";
+        discordPresence.smallImageText = "Rogue - Level 100";
+        Discord_UpdatePresence(&discordPresence);
+    }
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
