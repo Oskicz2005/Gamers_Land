1cebf4bfdae82230dd3ba32ca56ce489da08a09b
Oskicz2005 <44738487+Oskicz2005@users.noreply.github.com>
2018-11-04 04:02:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,14 @@
-const Discord = require('discord.js');
+	const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-static void UpdatePresence()
-    {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "Playing Solo";
-        discordPresence.details = "Competitive";
-        discordPresence.startTimestamp = 1507665886;
-        discordPresence.endTimestamp = 1507665886;
-        discordPresence.largeImageText = "Numbani";
-        discordPresence.smallImageText = "Rogue - Level 100";
-        discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-        discordPresence.partySize = 1;
-        discordPresence.partyMax = 5;
-        discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
-        discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-        Discord_UpdatePresence(&discordPresence);
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
 
